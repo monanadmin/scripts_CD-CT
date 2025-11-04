149f8936a5765fe85ee8c6ea86d897619e596a7b
guilherme.torresmendonca@ime.usp.br <guilherme.mendonca@headnode.egeon.cptec.inpe.br>
2025-11-04 15:51:13 -0300

    wip

--- scripts/5.run_post_on_mpas_grid.bash
+++ scripts/5.run_post_on_mpas_grid.bash
@@ -38,10 +38,10 @@ LON_MAX=-20
 V_MIN=90000
 V_MAX=103000
 # Input file from which variable should be extracted
-FILENAME=MONAN_DIAG_G_MOD_ERA5_2007062200_2007062400.00.00.lat_-35_lon_-55_oradius_2600_iradius_2000_margin_600_hres_50_lres_250.regionL55
+FILENAME=MONAN_DIAG_G_MOD_ERA5_2007062200_2007062400.00.00.lat_-35_lon_-55_oradius_2600_iradius_2000_margin_600_hres_50_lres_300.regionL55
 FILEPATH=${DATAOUT}/2007062200/Model/${FILENAME}.nc
 # File from which grid characteristics should be extracted
-GFILEPATH=${DATAOUT}/2007062200/Pre/lat_-35_lon_-55_oradius_2600_iradius_2000_margin_600_hres_50_lres_250.region.init.nc
+GFILEPATH=${DATAOUT}/2007062200/Pre/lat_-35_lon_-55_oradius_2600_iradius_2000_margin_600_hres_50_lres_300.region.init.nc
 # Output directory and filename to save plot
 POSTFILEDIR=${DATAOUT}/2007062200/Post
 POSTFILEPATH=${POSTFILEDIR}/${VAR}_${FILENAME}.png
@@ -51,5 +51,7 @@ if [ ! -d "${POSTFILEDIR}" ]; then
   mkdir "${POSTFILEDIR}"
 fi
 
+CONDA_PATH="$(conda info --root)"
+source "$CONDA_PATH/etc/profile.d/conda.sh"
 conda activate vtx_env
 python3 ${SOURCES}/CGFD-USP-Post-Proc/mpas_plot.py -f $FILEPATH -gf $GFILEPATH -vmin $V_MIN -vmax $V_MAX -v $VAR -lat_min $LAT_MIN -lat_max $LAT_MAX -lon_min $LON_MIN -lon_max $LON_MAX -o $POSTFILEPATH
