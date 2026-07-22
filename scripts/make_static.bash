1424de0d20928f03cf7752f55bfacf4986b1f241
egkhamis <egkhamis@gmail.com>
2026-07-22 19:01:37 +0000

    Revert "Merge branch 'feature/scripts_2.X' into develop"
    This reverts commit 34b0beea20c989038810775c78c8a54e36c749c8, reversing
    changes made to 1d4ffa04207a1f317e8330e78c680917cef0e48a.

--- scripts/make_static.bash
+++ scripts/make_static.bash
@@ -177,7 +177,6 @@ echo " "
 EOF0
 chmod a+x ${DIRRUN}/static.bash
 rm -fr ${DATAIN}/fixed/x1.${RES}.static.nc
-rm -fr ${DATAIN}/fixed/x1.${RES}.ugwp_oro_data.nc
 
 
 case "${SCHEDULER_SYSTEM}" in
@@ -209,15 +208,6 @@ then
    chmod 755 ${DATAIN}/fixed/*
 else
    echo -e  "${RED}==>${NC} File ${DIRRUN}/x1.${RES}.static.nc was not created. \n"
-   exit -1
-fi
-
-if [ -s ${DIRRUN}/x1.${RES}.ugwp_oro_data.nc ]
-then
-   mv ${DIRRUN}/x1.${RES}.ugwp_oro_data.nc ${DATAIN}/fixed
-   chmod 755 ${DATAIN}/fixed/*
-else
-   echo -e  "${RED}==>${NC} File ${DIRRUN}/x1.${RES}.ugwp_oro_data.nc was not created. \n"
    exit -1
 fi
 
